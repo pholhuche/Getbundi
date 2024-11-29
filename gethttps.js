ae6d2faf286ad69c8d0d3c64978be3b91438c602
pholhuche <ooseni27@gmail.com>
2024-11-29 14:04:16 +0100

    Modified 29Nov

---
+++ gethttps.js
@@ -0,0 +1,29 @@
+const req=new XMLHttpRequest()
+
+req.open("GET", "https://catfact.ninja/facts?limit=15&max_length=60")
+req.send()
+console.log(req.response)
+req.responseType="json"
+req.onload = ()=> console.log(req.response);
+;
+
+const getFacts = new Promise((resolve,reject)=>{
+    req.open("GET", "https://catfact.ninja/facts?limit=15&max_length=60")
+    req.send()
+    req.responseType="json"
+    req.onload = ()=> resolve(req.response);
+    req.onerror = ()=> reject(req.statusText);
+    ; 
+})
+
+getFacts
+.then(result=>result.data)
+.then(result=>console.log(result))
+.catch(error=>console.log(error))
+
+document.getElementById("fetchFacts").addEventListener("click", async () => {
+    (body.textContent=factsList);
+    // const factsList = document.getElementById("getFacts");
+    // factsList.innerHTML = "Loading...";
+})
+
